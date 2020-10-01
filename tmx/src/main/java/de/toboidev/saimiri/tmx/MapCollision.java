4e1d1867fc853d9a4d4d9d16064af3d8e0368f42
Eike Foede <toboi@toboidev.de>
2020-10-01 11:18:04 +0200

    Refactored MapCollision generator to enable custom layer filters

--- tmx/src/main/java/de/toboidev/saimiri/tmx/MapCollision.java
+++ tmx/src/main/java/de/toboidev/saimiri/tmx/MapCollision.java
@@ -7,9 +7,15 @@ import org.tiledreader.TiledTileLayer;
 
 public class MapCollision {
 
+    @Deprecated
     public static TileMap getTileMapCollider(TiledMap map) {
+        return getTileMapColliderLayerName(map, "[Collision]");
+    }
+
+    @Deprecated
+    public static TileMap getTileMapColliderLayerName(TiledMap map, String layerFilter) {
         if (map.getTileHeight() != map.getTileWidth()) {
-            throw new UnsupportedOperationException("Can at the moment only handle maps with equal tileHeight and tileWidth!");
+            throw new UnsupportedOperationException("Can at the moment only handle maps with square tiles (equal tileHeight and tileWidth)!");
         }
 
         int width = map.getWidth();
@@ -17,17 +23,41 @@ public class MapCollision {
         TileMap tm = new TileMap(map.getTileWidth(), width, height);
 
         for (TiledLayer layer : map.getNonGroupLayers()) {
-            if (layer.getName().contains("[Collision]")) {
-                if (layer instanceof TiledTileLayer) {
-                    for (int x = 0; x < width; x++) {
-                        for (int y = 0; y < height; y++) {
-                            tm.tileBlocking[x][y] |= ((TiledTileLayer) layer).getTile(x, height - 1 - y) != null;
-                        }
-                    }
-                }
+            if (layer.getName().contains(layerFilter)) {
+                fillCollisionMap(width, height, tm, layer);
             }
         }
         return tm;
 
+    }
+
+    public static TileMap getTileMapCollider(TiledMap map, String collisionLayer) {
+        if (map.getTileHeight() != map.getTileWidth()) {
+            throw new UnsupportedOperationException("Can at the moment only handle maps with square tiles (equal tileHeight and tileWidth)!");
+        }
+
+        int width = map.getWidth();
+        int height = map.getHeight();
+        TileMap tm = new TileMap(map.getTileWidth(), width, height);
+
+        for (TiledLayer layer : map.getNonGroupLayers()) {
+            Object layerCollision = layer.getProperty("collision");
+            if (layerCollision != null && layerCollision.equals(collisionLayer)) {
+                fillCollisionMap(width, height, tm, layer);
+            }
+        }
+        return tm;
+
+    }
+
+    private static void fillCollisionMap(int width, int height, TileMap tm, TiledLayer layer) {
+        if (layer instanceof TiledTileLayer) {
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    tm.tileBlocking[x][y] |= ((TiledTileLayer) layer).getTile(x, height - 1 - y) != null;
+                }
+            }
+
+        }
     }
 }
